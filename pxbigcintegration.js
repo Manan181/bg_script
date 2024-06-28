ce15d573df388f5eb551e7ecde262116481e9ea1
Manan Sonani <manansonani7979@gmail.com>
2024-06-28 15:41:16 +0530

    Create pxbigcintegration.js

---
+++ pxbigcintegration.js
@@ -0,0 +1,12 @@
+<script type="text/x-handlebars-template"> 
+    {{#each products.top_sellers}}
+      {{assignVar "pId" this.id}}
+      {{#each this.custom_fields}}
+        {{#if this.name "===" "enable_product_designer"}}
+          {{#if this.value "===" "true"}}
+            {{arrayify (getVar 'pId')}}
+          {{/if}}
+        {{/if}}
+      {{/each}}
+    {{/each}}
+</script>
