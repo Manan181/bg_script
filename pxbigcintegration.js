ddd5b2983852d83cf4b1ec501e1f7792886346d6
Manan Sonani <manansonani7979@gmail.com>
2024-07-01 14:59:16 +0530

    Update pxbigcintegration.js

--- pxbigcintegration.js
+++ pxbigcintegration.js
@@ -1,14 +1,28 @@
-alert('hello from the pxbigcintegration script');
 console.log('hello from the pxbigcintegration script');
-// <script type="text/x-handlebars-template"> 
-//     {{#each products.top_sellers}}
-//       {{assignVar "pId" this.id}}
-//       {{#each this.custom_fields}}
-//         {{#if this.name "===" "enable_product_designer"}}
-//           {{#if this.value "===" "true"}}
-//             {{arrayify (getVar 'pId')}}
-//           {{/if}}
-//         {{/if}}
-//       {{/each}}
-//     {{/each}}
-// </script>
+(function() {
+    // Get the script element
+    var scripts = document.getElementsByTagName('script');
+    var myScript = scripts[scripts.length - 1];
+    console.log(myScript, 'myScript');
+    // Get the query string from the script's src
+    var queryString = myScript.src.replace(/^[^\?]+\??/, '');
+
+    // Parse the query string into an object
+    var params = {};
+    queryString.split("&").forEach(function(param) {
+        var keyValue = param.split("=");
+        params[keyValue[0]] = decodeURIComponent(keyValue[1]);
+    });
+
+    // Now you can use the params object
+    console.log(params);
+
+    // Example: Access individual parameters
+    var param1 = params['param1'];
+    var param2 = params['param2'];
+    
+    console.log("Param1:", param1);
+    console.log("Param2:", param2);
+
+    // Use the parameters as needed in your script
+})();
