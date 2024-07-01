4816d861549387f98193d24de95a985a5ef1c7d3
Manan Sonani <manansonani7979@gmail.com>
2024-07-01 12:46:35 +0530

    Update pxbigcintegration.js

--- pxbigcintegration.js
+++ pxbigcintegration.js
@@ -1,12 +1,14 @@
-<script type="text/x-handlebars-template"> 
-    {{#each products.top_sellers}}
-      {{assignVar "pId" this.id}}
-      {{#each this.custom_fields}}
-        {{#if this.name "===" "enable_product_designer"}}
-          {{#if this.value "===" "true"}}
-            {{arrayify (getVar 'pId')}}
-          {{/if}}
-        {{/if}}
-      {{/each}}
-    {{/each}}
-</script>
+alert('hello from the pxbigcintegration script');
+console.log('hello from the pxbigcintegration script');
+// <script type="text/x-handlebars-template"> 
+//     {{#each products.top_sellers}}
+//       {{assignVar "pId" this.id}}
+//       {{#each this.custom_fields}}
+//         {{#if this.name "===" "enable_product_designer"}}
+//           {{#if this.value "===" "true"}}
+//             {{arrayify (getVar 'pId')}}
+//           {{/if}}
+//         {{/if}}
+//       {{/each}}
+//     {{/each}}
+// </script>
